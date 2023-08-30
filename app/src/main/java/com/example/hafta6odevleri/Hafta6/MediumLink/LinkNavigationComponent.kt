ee33050c90165cd3f5a1f3202f2ec41f741cd210
Döne Sakız <donesakiz0@gmail.com>
2023-08-30 23:55:38 +0300

    Add medium Link

--- app/src/main/java/com/example/hafta6odevleri/Hafta6/MediumLink/LinkNavigationComponent.kt
+++ app/src/main/java/com/example/hafta6odevleri/Hafta6/MediumLink/LinkNavigationComponent.kt
@@ -1,3 +1,5 @@
 package com.example.hafta6odevleri.Hafta6.MediumLink
 
 //Navigation Component konusunu anlattığım yazımın linki
+
+//https://medium.com/@doneesakizz/android-uygulamalarında-kolay-navigasyon-navigation-component-kullanım-rehberi-4762ebf27f1d
